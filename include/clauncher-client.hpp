adca824341e304e4663be272b0de5383a26e7301
Gleb Boiko <glebboyko@icloud.com>
2023-12-20 19:35:52 +0300

    Added launcher_client definitions

--- include/clauncher-client.hpp
+++ include/clauncher-client.hpp
@@ -1,2 +1,29 @@
 #pragma once
 
+#include <optional>
+
+#include "clauncher-supply.hpp"
+#include "tcp-client.hpp"
+
+namespace LNCR {
+
+class LauncherClient {
+ public:
+  LauncherClient(int port, logging_foo = LoggerCap);
+  ~LauncherClient();
+
+  bool LoadProcess(const std::string& bin_name,
+                   const ProcessConfig& process_config,
+                   bool wait_for_run) noexcept;
+  bool StopProcess(const std::string& bin_name, bool wait_for_stop) noexcept;
+  bool ReRunProcess(const std::string& bin_name, bool wait_for_rerun) noexcept;
+  bool IsProcessRunning(const std::string& bin_name) noexcept;
+  std::optional<int> GetProcessPid(const std::string& bin_name) noexcept;
+
+ private:
+  int port_;
+  logging_foo logger_;
+  std::optional<TCP::TcpClient> tcp_client_;
+};
+
+}  // namespace LNCR
